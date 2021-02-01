82565f72f134729e34b30dc6fcc3021cbfc6f984
naree <https://github.com/nareerat2530/Ithsdec2020_labb1.git>
2021-02-01 14:13:23 +0100

    lab1

---
+++ Iths_lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Iths_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string ");
+            var input = Console.ReadLine();
+            Console.WriteLine();
+            
+            long total = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var length = 1;
+
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    if (!int.TryParse(input[j].ToString(), out var num)) break;
+
+                    length++;
+
+                    if (input[i] != input[j]) continue;
+                    var substring = input.Substring(i, length);
+                    var substring2 = input.Substring(j+1);
+                    Console.Write(input.Substring(0, i));
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.Write(substring);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(substring2);
+                    Console.WriteLine();
+                    total += long.Parse(substring);
+                    break;
+
+                }
+            } 
+            Console.WriteLine($"\nTotal sum is {total} ");
+        }
+    }
+}
